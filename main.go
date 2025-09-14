6a86774283ceab405c8a6a1e160dbf4007a6314b
lenkton <lenktongm@gmail.com>
2025-09-14 17:52:09 +0300

    refactor: move handle offer into a separate module

--- main.go
+++ main.go
@@ -1,89 +1,14 @@
 package main
 
 import (
-	"encoding/json"
+	"echo-webrtc-test/pkg/services/users"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"net/http"
 
 	"github.com/pion/webrtc/v4"
 )
-
-var api *webrtc.API
-
-var remoteTrack *webrtc.TrackRemote
-var localTrack *webrtc.TrackLocalStaticRTP
-
-func handleOffer(w http.ResponseWriter, r *http.Request) {
-	pc, err := api.NewPeerConnection(webrtc.Configuration{})
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	pc.OnTrack(func(tr *webrtc.TrackRemote, r *webrtc.RTPReceiver) {
-		log.Println("INFO: wow, we have an audio track!")
-		remoteTrack = tr
-		// WARN: maybe it should go somewhere else...
-		//       we must be sure that we have both local
-		//       and remote streams at the same time
-		go func() {
-			for {
-				rtp, _, err := remoteTrack.ReadRTP()
-				if err == io.EOF {
-					log.Println("INFO: end of the remote track")
-					break
-				}
-				if err != nil {
-					log.Printf("ERROR: remoteTrack.ReadRTP: %v\n", err)
-					break
-				}
-				err = localTrack.WriteRTP(rtp)
-				if err != nil {
-					log.Printf("ERROR: localTrack.WriteRTP: %v\n", err)
-				}
-			}
-			err = pc.Close()
-			if err != nil {
-				log.Printf("ERROR: pc.Close: %v\n", err)
-			}
-			// TODO: do we need to stop the local track?
-		}()
-	})
-	var offer webrtc.SessionDescription
-	err = json.NewDecoder(r.Body).Decode(&offer)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	err = pc.SetRemoteDescription(offer)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	localTrack, _ = webrtc.NewTrackLocalStaticRTP(webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeOpus}, "audio", "shit")
-	pc.AddTrack(localTrack)
-
-	gatherPromise := webrtc.GatheringCompletePromise(pc)
-	answer, err := pc.CreateAnswer(nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	err = pc.SetLocalDescription(answer)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	<-gatherPromise
-	err = json.NewEncoder(w).Encode(*pc.LocalDescription())
-	if err != nil {
-		log.Println(err)
-		return
-	}
-}
 
 func main() {
 	settingEngine := webrtc.SettingEngine{}
@@ -107,10 +32,12 @@ func main() {
 	tcpMux := webrtc.NewICETCPMux(nil, tcpListener, 8)
 	settingEngine.SetICETCPMux(tcpMux)
 
-	api = webrtc.NewAPI(webrtc.WithSettingEngine(settingEngine))
+	api := webrtc.NewAPI(webrtc.WithSettingEngine(settingEngine))
+	usersService := users.NewService(api)
 
 	http.Handle("/", http.FileServer(http.Dir("./frontend")))
-	http.HandleFunc("POST /offer", handleOffer)
+	// TODO: move http-handling into some separate package
+	http.HandleFunc("POST /offer", usersService.HTTPHandleOffer)
 	log.Println("INFO: the app is listening on localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
---
+++ pkg/services/users/service.go
@@ -0,0 +1,90 @@
+package users
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/pion/webrtc/v4"
+)
+
+type UsersService struct {
+	api *webrtc.API
+}
+
+var remoteTrack *webrtc.TrackRemote
+var localTrack *webrtc.TrackLocalStaticRTP
+
+func NewService(api *webrtc.API) *UsersService {
+	return &UsersService{api: api}
+}
+
+func (us *UsersService) HTTPHandleOffer(w http.ResponseWriter, r *http.Request) {
+	pc, err := us.api.NewPeerConnection(webrtc.Configuration{})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	pc.OnTrack(func(tr *webrtc.TrackRemote, r *webrtc.RTPReceiver) {
+		log.Println("INFO: wow, we have an audio track!")
+		remoteTrack = tr
+		// WARN: maybe it should go somewhere else...
+		//       we must be sure that we have both local
+		//       and remote streams at the same time
+		go func() {
+			for {
+				rtp, _, err := remoteTrack.ReadRTP()
+				if err == io.EOF {
+					log.Println("INFO: end of the remote track")
+					break
+				}
+				if err != nil {
+					log.Printf("ERROR: remoteTrack.ReadRTP: %v\n", err)
+					break
+				}
+				err = localTrack.WriteRTP(rtp)
+				if err != nil {
+					log.Printf("ERROR: localTrack.WriteRTP: %v\n", err)
+				}
+			}
+			err = pc.Close()
+			if err != nil {
+				log.Printf("ERROR: pc.Close: %v\n", err)
+			}
+			// TODO: do we need to stop the local track?
+		}()
+	})
+	var offer webrtc.SessionDescription
+	err = json.NewDecoder(r.Body).Decode(&offer)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	err = pc.SetRemoteDescription(offer)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	localTrack, _ = webrtc.NewTrackLocalStaticRTP(webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeOpus}, "audio", "shit")
+	pc.AddTrack(localTrack)
+
+	gatherPromise := webrtc.GatheringCompletePromise(pc)
+	answer, err := pc.CreateAnswer(nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	err = pc.SetLocalDescription(answer)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	<-gatherPromise
+	err = json.NewEncoder(w).Encode(*pc.LocalDescription())
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
