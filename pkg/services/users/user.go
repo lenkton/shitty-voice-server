312028cb88785a244f8a49a98052f4bb6c31115d
Sergey Antonov <lenktongm@gmail.com>
2025-09-18 23:13:48 +0300

    refactor: move OnTrack logic into a separate function

--- pkg/services/users/user.go
+++ pkg/services/users/user.go
@@ -27,34 +27,36 @@ func (u *User) CreatePeerConnection(api *webrtc.API) error {
 	}
 	u.pc = pc
 
-	pc.OnTrack(func(tr *webrtc.TrackRemote, r *webrtc.RTPReceiver) {
-		log.Println("INFO: wow, we have an audio track!")
-		u.remoteTrack = tr
-		// WARN: maybe it should go somewhere else...
-		//       we must be sure that we have both local
-		//       and remote streams at the same time
-		go func() {
-			for {
-				rtp, _, err := u.remoteTrack.ReadRTP()
-				if err == io.EOF {
-					log.Println("INFO: end of the remote track")
-					break
-				}
-				if err != nil {
-					log.Printf("ERROR: remoteTrack.ReadRTP: %v\n", err)
-					break
-				}
-				err = u.localTrack.WriteRTP(rtp)
-				if err != nil {
-					log.Printf("ERROR: localTrack.WriteRTP: %v\n", err)
-				}
-			}
-			err = pc.Close()
-			if err != nil {
-				log.Printf("ERROR: pc.Close: %v\n", err)
-			}
-			// TODO: do we need to stop the local track?
-		}()
-	})
+	pc.OnTrack(u.onTrack)
 	return nil
+}
+
+func (u *User) onTrack(tr *webrtc.TrackRemote, r *webrtc.RTPReceiver) {
+	log.Println("INFO: wow, we have an audio track!")
+	u.remoteTrack = tr
+	// WARN: maybe it should go somewhere else...
+	//       we must be sure that we have both local
+	//       and remote streams at the same time
+	go func() {
+		for {
+			rtp, _, err := u.remoteTrack.ReadRTP()
+			if err == io.EOF {
+				log.Println("INFO: end of the remote track")
+				break
+			}
+			if err != nil {
+				log.Printf("ERROR: remoteTrack.ReadRTP: %v\n", err)
+				break
+			}
+			err = u.localTrack.WriteRTP(rtp)
+			if err != nil {
+				log.Printf("ERROR: localTrack.WriteRTP: %v\n", err)
+			}
+		}
+		err := u.pc.Close()
+		if err != nil {
+			log.Printf("ERROR: pc.Close: %v\n", err)
+		}
+		// TODO: do we need to stop the local track?
+	}()
 }
